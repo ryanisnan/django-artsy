de1ca225b0fe7c3b9f51d6b0d9b181f65e7e3a52
Ryan West <ryanisnan@gmail.com>
2010-12-04 16:09:51 -0500

    added docstrings to models.py

--- artsy/models.py
+++ artsy/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 import os.path
-	
+
+"""
+Determine the upload path for this particular project.
+
+An example upload path would be apps/artsy/my-project/image1.jpg
+
+The path, when used is added to the project's MEDIA_ROOT setting.
+"""
 def get_project_image_upload_path(instance, filename):
 	try:
 		path = os.path.join('apps/artsy', instance.slug, filename)
@@ -12,7 +19,10 @@ def get_project_image_upload_path(instance, filename):
 			pass
 	return path
 		
-
+"""
+A Category for projects to be filed under. Example categories might be
+Typography, Web, Print, Concept Art, etc.
+"""
 class Category(models.Model):
 	title = models.CharField(max_length=32)
 	slug = models.CharField(max_length=32, help_text='A slug is how this category is represented when in the URL.')
@@ -25,6 +35,11 @@ class Category(models.Model):
 	def __unicode__(self):
 		return self.title
 	
+"""
+A Project is the core object of artsy. A project represents a discrete
+unit of work. A project can belong to a category, and have a collection of
+images, as well as the standard attributes listed below.
+"""
 class Project(models.Model):
 	title = models.CharField(max_length=140)
 	slug = models.SlugField(max_length=140, help_text='A slug is how this project is represented when in the URL.')
@@ -41,7 +56,12 @@ class Project(models.Model):
 	
 	def __unicode__(self):
 		return self.title
-		
+
+"""
+An image associated to a project. A ProjectImage can have a thumbnail image
+as well as an actual image. ProjectImages can be ordered too on a per-project
+basis.
+"""		
 class ProjectImage(models.Model):
 	project = models.ForeignKey('Project')
 	thumbnail = models.ImageField(upload_to=get_project_image_upload_path, null=True, blank=True)
